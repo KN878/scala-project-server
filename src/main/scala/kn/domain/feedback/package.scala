c85c191c66e878f25735aa69e2ae518ff7162ccb
KN878 <kalinskiy98@gmail.com>
2020-04-28 19:00:06 +0300

    Added cast to secret customer feedback

--- src/main/scala/kn/domain/feedback/package.scala
+++ src/main/scala/kn/domain/feedback/package.scala
@@ -3,17 +3,31 @@ package kn.domain
 import java.time.Instant
 
 package object feedback {
-  implicit def createFeedbackRequestToFeedback(
-      request: CreateFeedbackRequest,
-  )(implicit customerId: Long): FeedbackWithIds =
-    FeedbackWithIds(
-      None,
-      request.shopId,
-      customerId,
-      request.pros,
-      request.cons,
-      request.additionalInfo,
-      Instant.now(),
-      FeedbackType.Feedback,
-    )
+  implicit class FeedbackSyntax(request: CreateFeedbackRequest) {
+    def toFeedback(implicit customerId: Long): FeedbackWithIds =
+      FeedbackWithIds(
+        None,
+        request.shopId,
+        customerId,
+        request.pros,
+        request.cons,
+        request.additionalInfo,
+        Instant.now(),
+        FeedbackType.Feedback,
+      )
+  }
+
+  implicit class SecretCustomerSyntax(request: CreateFeedbackRequest) {
+    def toSecretCustomer(implicit customerId: Long): FeedbackWithIds =
+      FeedbackWithIds(
+        None,
+        request.shopId,
+        customerId,
+        request.pros,
+        request.cons,
+        request.additionalInfo,
+        Instant.now(),
+        FeedbackType.SecretCustomer,
+      )
+  }
 }
