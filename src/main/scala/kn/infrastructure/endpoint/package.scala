4e56170863c01585937e24c78117e4594085e541
KN878 <kalinskiy98@gmail.com>
2020-04-28 19:00:30 +0300

    Moved Instant encoder to package

--- src/main/scala/kn/infrastructure/endpoint/package.scala
+++ src/main/scala/kn/infrastructure/endpoint/package.scala
@@ -1,5 +1,8 @@
 package kn.infrastructure
 
+import java.time.{Instant, ZoneId}
+
+import io.circe.Encoder
 import kn.domain.users.User
 import org.http4s.Response
 import tsec.authentication.{AugmentedJWT, SecuredRequest, TSecAuthService}
@@ -8,4 +11,8 @@ package object infrastructure {
   type AuthService[Auth, F[_]] = TSecAuthService[User, AugmentedJWT[Auth, Long], F]
   type AuthEndpoint[Auth, F[_]] =
     PartialFunction[SecuredRequest[F, User, AugmentedJWT[Auth, Long]], F[Response[F]]]
+
+  implicit val instantEncoder: Encoder[Instant] =
+    Encoder.encodeString.contramap[Instant](_.atZone(ZoneId.of("Europe/Moscow")).toString)
+
 }
