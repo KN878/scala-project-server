028d21a64425fb4abf40cf6f4cb40fb18df5f05c
KN878 <kalinskiy98@gmail.com>
2020-04-28 19:05:05 +0300

    Secret customer endpoints

--- src/main/scala/kn/Server.scala
+++ src/main/scala/kn/Server.scala
@@ -1,5 +1,6 @@
 package kn
 
+import cats.Monad
 import cats.data.Kleisli
 import cats.effect._
 import cats.implicits._
@@ -8,17 +9,23 @@ import doobie.util.ExecutionContexts
 import io.circe.config.parser
 import kn.config.{DatabaseConfig, MobileServerConfig}
 import kn.domain.authentication.Auth
-import kn.domain.secretShopper.actions.{ActionService, ActionValidationInterpreter}
-import kn.domain.shops.{ShopService, ShopValidationInterpreter}
+import kn.domain.feedback.FeedbackRepository
+import kn.domain.secretCustomer.actions.{
+  ActionRepository,
+  ActionService,
+  ActionValidationInterpreter,
+}
+import kn.domain.secretCustomer.{SecretCustomerService, SessionValidationInterpreter}
+import kn.domain.shops.{ShopRepository, ShopService, ShopValidationInterpreter}
 import kn.domain.transactions.{TransactionService, TransactionValidationInterpreter}
-import kn.domain.users.{User, UserService, UserValidationInterpreter}
-import kn.infrastructure.doobie.{DoobieActionsRepositoryInterpreter, DoobieAuthRepositoryInterpreter, DoobieFeedbackRepositoryInterpreter, DoobieShopRepositoryInterpreter, DoobieUserRepositoryInterpreter}
+import kn.domain.users._
+import kn.infrastructure.doobie._
 import kn.infrastructure.endpoint._
 import monix.eval.{Task, TaskApp}
-import org.http4s.{Request, Response}
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.{Router, Server => H4Server}
+import org.http4s.{Request, Response}
 import tsec.authentication.{AugmentedJWT, IdentityStore, SecuredRequestHandler}
 import tsec.mac.jca.HMACSHA256
 import tsec.passwordhashers.jca.BCrypt
@@ -44,7 +51,42 @@ object Server extends TaskApp {
       routeAuth = SecuredRequestHandler(authenticator)
     } yield routeAuth
 
-  def router[F[_]: Async: ContextShift](conf: MobileServerConfig): Resource[F, Kleisli[F, Request[F], Response[F]]] =
+  def initTransactionService[F[_]: Monad](
+      shopRepo: ShopRepository[F],
+      userRepo: UserRepository[F],
+      userValidation: UserValidationAlgebra[F],
+  ): TransactionService[F] = {
+    val transactionValidation = TransactionValidationInterpreter[F](shopRepo, userRepo)
+    TransactionService[F](
+      shopRepo,
+      userRepo,
+      transactionValidation,
+      userValidation,
+    )
+  }
+
+  def initSecretCustomerService[F[_]: Async: Clock: Monad](
+      shopRepo: ShopRepository[F],
+      actionsRepo: ActionRepository[F],
+      feedbackRepo: FeedbackRepository[F],
+      transactionService: TransactionService[F],
+      chargeAmount: Float,
+      xa: HikariTransactor[F],
+  ): SecretCustomerService[F] = {
+    val sessionRepo = DoobieSecretCustomerSessionInterpreter[F](xa)
+    val sessionValidation = SessionValidationInterpreter[F](sessionRepo, shopRepo, actionsRepo)
+    SecretCustomerService[F](
+      sessionRepo,
+      feedbackRepo,
+      transactionService,
+      sessionValidation,
+      chargeAmount,
+    )
+  }
+
+  def router[F[_]: Async: ContextShift: Clock](
+      conf: MobileServerConfig,
+  ): Resource[F, Kleisli[F, Request[F], Response[F]]] =
     for {
       xa <- transactor[F](conf)
       userRepo = DoobieUserRepositoryInterpreter[F](xa)
@@ -53,17 +95,19 @@ object Server extends TaskApp {
       shopRepo = DoobieShopRepositoryInterpreter[F](xa)
       shopValidation = ShopValidationInterpreter[F](shopRepo)
       shopService = ShopService[F](shopRepo, shopValidation)
-      transactionValidation = TransactionValidationInterpreter[F](shopRepo, userRepo)
-      transactionService = TransactionService(
-        shopRepo,
-        userRepo,
-        transactionValidation,
-        userValidation,
-      )
       feedbackRepo = DoobieFeedbackRepositoryInterpreter[F](xa)
       actionsRepo = DoobieActionsRepositoryInterpreter[F](xa)
       actionsValidation = ActionValidationInterpreter[F](actionsRepo, shopRepo)
-      actionsService = ActionService(actionsRepo, actionsValidation)
+      actionsService = ActionService[F](actionsRepo, actionsValidation)
+      transactionService = initTransactionService[F](shopRepo, userRepo, userValidation)
+      secretCustomerService = initSecretCustomerService[F](
+        shopRepo,
+        actionsRepo,
+        feedbackRepo,
+        transactionService,
+        conf.secretCustomer.charge,
+        xa
+      )
       routeAuth <- authenticator[F](xa, userRepo)
       httpApp = Router(
         "/users" -> {
@@ -80,9 +124,12 @@ object Server extends TaskApp {
         "/feedback" -> routeAuth.liftService(
           FeedbackEndpoints[F, BCrypt, HMACSHA256](feedbackRepo),
         ),
-        "secretCustomerActions" -> routeAuth.liftService(
-          ActionsEndpoints[F, BCrypt, HMACSHA256](actionsService)
+        "/secretCustomer/actions" -> routeAuth.liftService(
+          ActionsEndpoints[F, BCrypt, HMACSHA256](actionsService),
         ),
+        "/secretCustomer/session" -> routeAuth.liftService(
+          SecretCustomerSessionEndpoints[F, BCrypt, HMACSHA256](secretCustomerService)
+        )
       ).orNotFound
     } yield httpApp
 
